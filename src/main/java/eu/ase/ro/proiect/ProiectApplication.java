af7f9826f9a27e5d4f20fa5254df20554bf017e4
Andrii Alexandru-Marian <alexandru.andrii@concept24.ro>
2025-01-11 12:49:14 +0200

    test commit

--- src/main/java/eu/ase/ro/proiect/ProiectApplication.java
+++ src/main/java/eu/ase/ro/proiect/ProiectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProiectApplication {
 
 	public static void main(String[] args) {
+//		test commit
 		SpringApplication.run(ProiectApplication.class, args);
 	}
 
